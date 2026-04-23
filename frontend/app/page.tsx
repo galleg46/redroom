556c3aedeeafe23186a7e1c118c3b21aa0ac1fcd
Anthony Gallegos <galleg46@uwm.edu>
2026-04-23 01:54:23 -0500

    Moved the Header component to layout.tsx
    Because it is a component that will be used across all pages, I moved it to the app level layout.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,10 +3,7 @@ import Header from "@/app/components/header";
 export default function HomePage() {
 
   return (
-      <main className="bg-black flex min-h-screen flex-col p-6">
-          <div>
-              <Header />
-          </div>
+      <main className="flex min-h-screen flex-col p-6">
       </main>
   );
 }
