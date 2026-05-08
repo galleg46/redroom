a735b3979741e0ae65bb64b82ef5505a45c28e72
Anthony Gallegos <galleg46@uwm.edu>
2026-05-08 11:23:08 -0500

    Added button to the homepage that takes users to the waiver page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { CustomButton } from "@/app/components/ui/CustomButton";
+
 export default function HomePage() {
 
   return (
       <main className="flex min-h-screen flex-col p-6">
+          <div className="flex flex-col items-center justify-center">
+              <p>First Time attending an event at Red Room? Please fill out the waiver below.</p>
+              <div className="mt-4">
+                  <CustomButton variant="contained" href="/waiver">Waiver</CustomButton>
+              </div>
+          </div>
       </main>
   );
 }
