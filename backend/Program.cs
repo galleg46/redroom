25e062809e395d5daabd771936d2768cf8fc5dcb
Anthony Gallegos <galleg46@uwm.edu>
2026-06-26 13:46:40 -0500

    more configs
    added hangfire config and cron job

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,16 +1,22 @@
 using backend.dbcontext;
 using backend.interfaces;
+using backend.models.config;
 using backend.service;
+using Hangfire;
+using Hangfire.PostgreSql;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IWaiverService, WaiverService>();
 builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IEventReminderService, EventReminderService>();
 
 builder.Services.AddCors(options =>
 {
@@ -23,9 +29,24 @@ builder.Services.AddCors(options =>
         });
 });
 
+builder.Services.AddHangfire(config =>
+{
+    config.UsePostgreSqlStorage(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
 app.MapControllers();
+app.UseHangfireDashboard();
+
+RecurringJob.AddOrUpdate<EventReminderService>("email-invite", 
+    service => service.SendEventReminders(), 
+    Cron.Daily(10),
+    new RecurringJobOptions
+    {
+        TimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Chicago")
+    });
 
 app.Run();
