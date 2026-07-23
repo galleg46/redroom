c054df3200b6eae260fe645432f2036d5dd7f428
Anthony Gallegos <galleg46@uwm.edu>
2026-07-23 12:41:15 -0500

    wrapped application in a AppRouterCacheProvider to solve emotion hydration errors

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,44 +1,45 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./ui/globals.css";
 import Header from "@/app/components/header";
 import {Box} from "@mui/material";
+import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
 
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Red Room",
-  description: "",
+    title: "Red Room",
+    description: "",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="bg-black">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Box sx={{
-            minHeight: "100vh",
-            color: "white",
-            backgroundImage: "linear-gradient(#000000 10%, #1a0000 20%, #4a0000 45%, #8b0000 100%)",
-        }}>
-            <Header />
-            {children}
-        </Box>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="bg-black">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AppRouterCacheProvider>
+            <Box sx={{
+                minHeight: "100vh",
+                color: "white",
+                backgroundImage: "linear-gradient(#000000 10%, #1a0000 20%, #4a0000 45%, #8b0000 100%)",
+            }}>
+                <Header/>
+                {children}
+            </Box>
+        </AppRouterCacheProvider>
+        </body>
+        </html>
+    );
 }
